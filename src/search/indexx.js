b5fc64ab4f99349144171fae912eb9a259d0d46c
Tushar1271 <tushar1271.cse18@chitkara.edu.in>
2021-05-06 07:36:17 +0530

    First commit

---
+++ src/search/indexx.js
@@ -0,0 +1,24 @@
+import React,{ useState} from 'react';
+import './style.css';
+import List from './list.json';
+export default function Main(){
+    const [searchTerm, setSearchTerm]=useState("");
+      return <div className="main">
+       <input placeholder="Enter Employee Name ....." onChange={(event)=>{
+           setSearchTerm(event.target.value);
+       }}></input>
+    <table>
+{List.filter((val)=>{
+    if(searchTerm===''){
+        return val;
+    }
+    else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val;
+    }
+}).map((val,key)=>{
+    return (<tr><td>{val.name}</td><td>{val.location}</td></tr>)
+    
+})}
+</table>
+</div>
+}
